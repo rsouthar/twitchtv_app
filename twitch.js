94257947463f308d7b1d199d240fb4d220af44d3
Rick <rdsouthard1979@gmail.com>
2018-03-07 18:21:20 -0600

    Getting more done little by little.

--- twitch.js
+++ twitch.js
@@ -1,5 +1,8 @@
+
+
 $(document).ready(function() {
-  var url = "https://wind-bow.glitch.me/twitch-api/streams/freecodecamp";
+  var following = [];
+  var url = "https://wind-bow.glitch.me/twitch-api/channels/freecodecamp/?callback=?";
   $.getJSON(url,function(data1){
     if(data1.stream===null){
       $("#fccStatus").html("Free Code Camp is offline");
@@ -7,4 +10,16 @@ $(document).ready(function() {
       $("#fccStatus").html("Free Code Camp is online");
     }
   });
+var followerURL = "https://api.twitch.tv/kraken/users/freecodecamp/follows/channels/?callback=?"
+$.getJSON(followerURL, function(data2) {
+  for (var i = 0; i < data2.follows.length; i++) {
+    var displayName = data2.follows[i].channel.display_name;
+    following.push(displayName);
+  }
+  following.push('conster404');
+  following.push('brunofin');
+  following.push('ESL_SC2');
+  console.log(following);
+});
+
 });
