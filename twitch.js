213d345b39ef6ed588d03e48c819154cef26a0e6
Rick <rdsouthard1979@gmail.com>
2018-03-10 10:53:05 -0600

    Things are working, but not yet fully ready.

--- twitch.js
+++ twitch.js
@@ -1,8 +1,8 @@
-
+var following = [];
 
 $(document).ready(function() {
-  var following = [];
-  var url = "https://wind-bow.glitch.me/twitch-api/channels/freecodecamp/?callback=?";
+
+  var url = "https://wind-bow.glitch.me/twitch-api/users/freecodecamp";
   $.getJSON(url,function(data1){
     if(data1.stream===null){
       $("#fccStatus").html("Free Code Camp is offline");
@@ -10,16 +10,21 @@ $(document).ready(function() {
       $("#fccStatus").html("Free Code Camp is online");
     }
   });
-var followerURL = "https://api.twitch.tv/kraken/users/freecodecamp/follows/channels/?callback=?"
-$.getJSON(followerURL, function(data2) {
-  for (var i = 0; i < data2.follows.length; i++) {
-    var displayName = data2.follows[i].channel.display_name;
-    following.push(displayName);
-  }
-  following.push('conster404');
-  following.push('brunofin');
-  following.push('ESL_SC2');
-  console.log(following);
-});
+  var followerURL = "https://wind-bow.glitch.me/twitch-api/users/freecodecamp/follows/channels/"
+  $.getJSON(followerURL, function(data2) {
+    for (var i = 0; i < data2.follows.length; i++) {
+      var displayName = data2.follows[i].channel.display_name;
+      var logo = data2.follows[i].channel.logo;
+      var status = data2.follows[i].channel.status;
 
+      
+      $('#followerInfo').prepend("<div class='row channel'>" +
+      "<div class='col-md-3'>" +
+        "<a href='https://www.twitch.tv/"+ displayName+"'><img src='" + logo + "'></a>" +
+      "</div>" +
+      "<div class='col-md-4'><span class='displayName text-center'>" + displayName + "</span></div>" +
+      "<div class='col-md-5'><span class='status'>" + status +
+      "</div></div>");
+    }
+  });
 });
